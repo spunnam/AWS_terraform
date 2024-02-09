449c970ed1fba0579f33efbe945fbd967f8faa61
spunnam <spunnam@cougarnet.uh.edu>
2024-02-09 12:08:24 -0600

    security group change

--- main.tf
+++ main.tf
@@ -1,9 +1,10 @@
 # main.tf
 resource "aws_instance" "ec2_instance" {
-  ami           = "ami-0a346c29399cd4934"
-  subnet_id     = "subnet-0ad8498fde7ac062d"
-  instance_type = "t2.micro"
-  key_name      = "EC2 Practice"
+  ami                    = "ami-0a346c29399cd4934"
+  subnet_id              = "subnet-0ad8498fde7ac062d"
+  instance_type          = "t2.micro"
+  key_name               = "EC2 Practice"
+  vpc_security_group_ids = ["sg-095763b3c130900bb"]
   tags = {
     Name = "test instance"
   }
@@ -14,7 +15,7 @@ resource "aws_security_group" "sg" {
   }
 }
 
-resource "aws_network_interface_sg_attachment" "sg_attachment" {
-  security_group_id    = "sg-095763b3c130900bb"
-  network_interface_id = "eni-023ef9e34a6d572c8"
-}
+# resource "aws_network_interface_sg_attachment" "sg_attachment" {
+#   security_group_id    = "sg-095763b3c130900bb"
+#   network_interface_id = "eni-023ef9e34a6d572c8"
+# }
