4613337d916c67502500d0f0d3f52c24583b3d0f
spunnam <spunnam@cougarnet.uh.edu>
2024-02-13 05:18:50 -0600

    minor change

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ resource "aws_instance" "ec2_instance" {
     yum install -y aws-cli
     
     # Download the file from S3 bucket
-    aws s3 cp s3://demo-bucket-891377224694/index /home/ec2-user/index
+    aws s3 cp s3://demo-bucket-891377224694/index.html /home/ec2-user/index.html
     
     # Install httpd (Linux 2 version)
     yum update -y
@@ -29,7 +29,7 @@ resource "aws_instance" "ec2_instance" {
     rm /var/www/html/index.html
     
     # Move the downloaded index file to the web server directory
-    mv /home/ec2-user/index /var/www/html/
+    mv /home/ec2-user/index.html /var/www/html/
     
     # Restart httpd for changes to take effect
     systemctl restart httpd
