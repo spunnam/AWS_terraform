02308fc4a003e83f237026492e766bf29e5aefec
spunnam <spunnam@cougarnet.uh.edu>
2024-02-13 05:07:58 -0600

    minor change

--- main.tf
+++ main.tf
@@ -8,17 +8,33 @@ resource "aws_instance" "ec2_instance" {
   vpc_security_group_ids = ["sg-095763b3c130900bb"]
 
   user_data = <<-EOF
-  #!/bin/bash
-  # Use this for your user data (script from top to bottom)
-  # install httpd (Linux 2 version)
-  yum update -y
-  yum install -y httpd
-  systemctl start httpd
-  systemctl enable httpd
-  sudo rm /var/www/html/index.html
-  aws s3 cp s3://demo-bucket-891377224694/index.html index.html
-  sudo mv /home/ec2-user/index.html /var/www/html/
+    #!/bin/bash
+    # Use this for your user data (script from top to bottom)
+    
+    # Install AWS CLI
+    yum install -y aws-cli
+    
+    # Download the file from S3 bucket
+    aws s3 cp s3://demo-bucket-891377224694/index /home/ec2-user/index
+    
+    # Install httpd (Linux 2 version)
+    yum update -y
+    yum install -y httpd
+    
+    # Start and enable httpd service
+    systemctl start httpd
+    systemctl enable httpd
+    
+    # Remove the default index.html
+    rm /var/www/html/index.html
+    
+    # Move the downloaded index file to the web server directory
+    mv /home/ec2-user/index /var/www/html/
+    
+    # Restart httpd for changes to take effect
+    systemctl restart httpd
   EOF
+
 
   tags = {
     Name = "new_terraform_instance"
